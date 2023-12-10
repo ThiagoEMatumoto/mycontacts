977e3990f456212f621d4079d9309b1a3cd28988
Thiago E Matumoto <thiagoeijimatumoto@gmail.com>
2023-12-09 22:27:10 -0300

    🎉 First commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+
+const app = express();
+
+app.get('/', (requeste, response) => {
+  response.send("Hello, World!")
+})
+
+
+app.listen(3000, () => console.log(`Server started at http://localholt:3000`))
